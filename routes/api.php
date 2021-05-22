a25baa7135bf0a4ce9f02942037f34c045d96f2f
sandipsilwal <sandip.silwal.ss@gmail.com>
2021-05-22 13:04:22 +0545

    Created Model -a

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::apiResource('/product','App\Http\Controllers\ApiControllers\ProductController');
+Route::group(['prefix'=>'product'], function(){
+    Route::apiResource('{product_id}/review','App\Http\Controllers\ApiControllers\ReviewController');
+
+});
